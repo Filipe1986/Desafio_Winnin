0e4b96ee36ba842e0dd8fe8665cbb9ab4fb946fd
Filipe <>
2020-05-31 22:32:30 -0300

    Removendo arquivos estranhos ao projeto, correção e identação do api.js

--- routes/api.js
+++ routes/api.js
@@ -1,67 +1,84 @@
 var express = require('express');
 var router = express.Router();
 var database = require('../config/database');
-var moment = require('moment'); 
+var moment = require('moment');
 
 
-router.get('/postagens', async  function(req, res, next) {
-  console.log(moment(req.query.dataInicia).valueOf())
-  try{
-    if(req.query.ordem == "ups" && req.query.dataInicial  && req.query.dataFinal){
-      sql  = 'select * from postagem where criacao >= \''+ req.query.dataInicial +'\' and criacao <= \''+ req.query.dataFinal+ '\' order by NUMBER_UPS desc';
-      var result = await  database.simpleExecute(sql);
-      usuarios = result.rows.map(el=>{
-        TITULO              : el.TITULO             
-        AUTOR               : el.AUTOR                
-        CRIACAO             : el.CRIACAO            
-        NUMBER_UPS          : el.NUMBER_UPS         
-        NUMBER_COMENTARIOS  : el.NUMBER_COMENTARIOS 
+router.get('/postagens', async function (req, res, next) {
+  inicial = moment(req.query.dataInicial).valueOf() / 1000;
+  final = moment(req.query.dataFinal).valueOf() / 1000;
+  var usuarios;
 
-      })
-    }else if(req.query.ordem == "comentarios"  && req.query.dataInicial  && req.query.dataFinal){
-      sql = 'select * from postagem where criacao >= \''+ req.query.dataInicial +'\' and criacao <= \''+ req.query.dataFinal +'\' order by NUMBER_COMENTARIOS desc'
-      var result = await  database.simpleExecute(sql);
-      usuarios = result.rows.map(el=>{
-        TITULO              : el.TITULO             
-        AUTOR               : el.AUTOR                
-        CRIACAO             : el.CRIACAO            
-        NUMBER_UPS          : el.NUMBER_UPS         
-        NUMBER_COMENTARIOS  : el.NUMBER_COMENTARIOS 
+  try {
+    if (req.query.ordem == "ups" && inicial && final) {
+      sql = 'select * from postagem where criacao >= '
+        + inicial
+        + ' and criacao <= '
+        + final
+        + ' order by NUMBER_UPS desc';
+      var result = await database.simpleExecute(sql);
+      usuarios = result.rows.map(el => ({
+        TITULO: el.TITULO,
+        AUTOR: el.AUTOR,
+        CRIACAO: moment(el.CRIACAO * 1000).format(),
+        NUMBER_UPS: el.NUMBER_UPS,
+        NUMBER_COMENTARIOS: el.NUMBER_COMENTARIOS
 
-      })
+      }));
+    } else if (req.query.ordem == "comentarios" && inicial && final) {
+      sql = 'select * from postagem where criacao >= '
+        + inicial + ' and criacao <= '
+        + final +
+        ' order by NUMBER_COMENTARIOS desc'
+      var result = await database.simpleExecute(sql);
+      usuarios = result.rows.map(el => ({
+        TITULO: el.TITULO,
+        AUTOR: el.AUTOR,
+        CRIACAO: moment(el.CRIACAO * 1000).format(),
+        NUMBER_UPS: el.NUMBER_UPS,
+        NUMBER_COMENTARIOS: el.NUMBER_COMENTARIOS
+
+      }));
     }else{
       res.status(400)
       res.json()
     }
 
-  }catch(err){
+  }catch(err) {
     console.log(err)
     res.status(500)
     res.json()
   }
-
   res.json(usuarios)
 });
 
-router.get('/usuarios', async function(req, res, next) {
-  try{
-    if(req.query.ordem == "ups"){
-      var result = await  database.simpleExecute('select * from postagem order by NUMBER_UPS desc');
-      usuarios = result.rows.map(el=>({
-        TITULO             : el.TITULO,             
-        AUTOR               : el.AUTOR ,               
-        CRIACAO             : moment(el.CRIACAO * 1000).format(),           
-        NUMBER_UPS          : el.NUMBER_UPS,         
-        NUMBER_COMENTARIOS  : el.NUMBER_COMENTARIOS 
+router.get('/usuarios', async function (req, res, next) {
+  try {
+    if (req.query.ordem == "ups") {
+      var result = await database.simpleExecute('select * from postagem order by NUMBER_UPS desc');
+      usuarios = result.rows.map(el => ({
+        TITULO: el.TITULO,
+        AUTOR: el.AUTOR,
+        CRIACAO: moment(el.CRIACAO * 1000).format(),
+        NUMBER_UPS: el.NUMBER_UPS,
+        NUMBER_COMENTARIOS: el.NUMBER_COMENTARIOS
 
       }));
-    }else if(req.query.ordem == "comentarios"){
-      var usuarios = await  database.simpleExecute('select * from postagem order by NUMBER_COMENTARIOS desc');
-    }else{
+    } else if (req.query.ordem == "comentarios") {
+      var result = await database.simpleExecute('select * from postagem order by NUMBER_COMENTARIOS desc');
+      usuarios = result.rows.map(el => ({
+        TITULO: el.TITULO,
+        AUTOR: el.AUTOR,
+        CRIACAO: moment(el.CRIACAO * 1000).format(),
+        NUMBER_UPS: el.NUMBER_UPS,
+        NUMBER_COMENTARIOS: el.NUMBER_COMENTARIOS
+
+      }));
+    } else {
       res.status(400)
     }
 
-  }catch{
+  } catch{
     res.status(400)
     res.json()
   }
--- test.js
+++
@@ -1,5 +0,0 @@
-var moment = require('moment'); 
-n = moment(1590935677*1000)
-console.log(n)
-
-
